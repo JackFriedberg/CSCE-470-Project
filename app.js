617c0676a745457e7358658132dfc7c12c3b4906
Jack Friedberg <jackfriedberg@gmail.com>
2019-11-10 16:09:25 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+
+const SolrNode = require('solr-node');
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+async function main() {
+    makeWebpage();
+}
+
+
+async function makeWebpage(){
+
+    app.use(express.static(__dirname + '/public'));
+
+    io.on('connection', function(socket){
+        socket.on('search', function(searchText){
+            searchSolr(searchText);
+        });
+    });
+
+    const port = process.env.PORT || 3000;
+    http.listen(port, function(){
+        console.log('listening on *:3000');
+    });
+}
+
+async function searchSolr(searchText){
+
+    //console.log("Searching for: " + searchText)
+    var strQuery = solrClient.query().q(searchText);
+    solrClient.search(strQuery, function (err, result) {
+        if (err) {
+           console.log(err);
+           return;
+        }
+        //console.log(result)
+        io.emit('sendResults', result);
+     });
+}
+
+var solrClient = new SolrNode({
+    host: '127.0.0.1',
+    port: '8983',
+    core: 'NBA_shard1_replica_n2',
+    protocol: 'http'
+});
+
+main();
